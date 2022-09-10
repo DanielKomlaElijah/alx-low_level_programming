7f6377cc582d53d5486088e223a1bb3752a3d08b
Daniel Komla Elijah <elijahdanielson@gmail.com>
2022-09-09 17:22:23 -0700

    A program that prints all possible different combinations of two digits.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations
+ *
+ * Description: program that prints the alphabet
+ * in lowercase, followed by a new line
+ *
+ * Return: 0 for success
+ *
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
