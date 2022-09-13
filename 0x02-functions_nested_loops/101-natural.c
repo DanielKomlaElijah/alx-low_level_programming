b1d21d99854b0358b1913c0075d83986852dfc12
Daniel Komla Elijah <elijahdanielson@gmail.com>
2022-09-13 16:51:22 -0700

    A function that list all the natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints
+ * all natural numbers
+ *
+ * Description: print all natural numbers
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			j += i;
+	}
+
+	printf("%d\n", b);
+	return (0);
+}
